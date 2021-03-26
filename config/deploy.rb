01992f109ed42532faa4609ae2a6b78fc7e73d4d
emmatshi-stack <tshibangubiayi@gmail.com>
2021-03-26 15:02:36 -0400

    branch to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,9 @@ lock "~> 3.15.0"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "emmanueltshibangu"
-set :repo_url, "https://github.com/Week-7-Team-4/Rocket_Elevators_API.git"
+set :repo_url, "https://github.com/emmatshi-stack/Rocket-levator-Foundation"
 # set :linked_files, %w{config/application.yml}
-# Default branch is :master
+Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
